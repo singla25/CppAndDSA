d1f236d90e2aa851db3a40be386d3fc6441e1553
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-21 23:42:11 +0530

    commit

---
+++ 04_patterns.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // int n;
+    // cout << "Enter a Number: ";
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n) {
+    //     int j = 1;
+    //     while (j <= n) {
+    //         cout << n-j+1 << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter no. of Columns: ";
+    // cin >> n;
+    // int i = 1;
+    // int count = 1;
+    // while (i<=n) {
+    //     int j = 1;
+    //     while(j<=n) {
+    //         cout << count << ' ';
+    //         count++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter no. of row of triangle: ";
+    // cin >> n;
+    // int i = 1;
+    // while (i<=n) {
+    //     int j = i;
+    //     while (j<=n) {
+    //         cout << "* ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int row = 1;
+    // while (row<=n) {
+    //     int col = 1;
+    //     while (col<=row) {
+    //         cout << "* ";
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=i) {
+    //         cout << i << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // int count = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=i) {
+    //         cout << count << ' ';
+    //         count++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     int value = i;
+    //     while (j<=i) {
+    //         cout << value << ' ';
+    //         value++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=i) {
+    //         cout << i+j-1 << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     int value = i;
+    //     while (j<=i) {
+    //         cout << value << ' ';
+    //         value--;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=i) {
+    //         cout << i-j+1 << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     char ch = 'A' + j - 1;
+    //     while (j<=n) {
+    //         cout << ch << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=n) {
+    //         char ch = 'A' + j - 1;
+    //         cout << ch << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int i = 1;
+    // char ch = 'A';
+    // while(i<=n){
+    //     int j = 1;
+    //     while (j<=n) {
+    //         cout << ch << ' ';
+    //         ch++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter your Number: ";
+    // cin >> n;
+    // int i = 1;
+    // while (i<=n) {
+    //     int j = 1; 
+    //     while (j<=n) {
+    //         char value = 'A'+ i + j - 2;
+    //         cout << value << ' ';
+    //         value++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter your Number: ";
+    // cin >> n;
+    // int i = 1;
+    // while (i<=n) {
+    //     int j = 1; 
+    //     char value = 'A'+ i + j - 2;
+    //     while (j<=n) {
+    //         cout << value << ' ';
+    //         value++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter your Number: ";
+    // cin >> n;
+    // int i = 1;
+    // while (i<=n) {
+    //     int j = 1;
+    //     while (j<=i) {
+    //         char ch = 'A' + i - 1;    
+    //         cout << ch << ' ';
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // int n;
+    // cout << "Enter your Number: ";
+    // cin >> n;
+    // int i = 1;
+    // char ch = 'A';
+    // while (i<=n) {
+    //     int j = 1;
+    //     while (j<=i) {
+    //         cout << ch << ' ';
+    //         ch++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    int n;
+    cout << "Enter your Number: ";
+    cin >> n;
+    int i = 1;
+    while (i<=n) {
+        int j = 1;
+        while (j<=i) {
+            cout << i << ' ';
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    
+
+} 
